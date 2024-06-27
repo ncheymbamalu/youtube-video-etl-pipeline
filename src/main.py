45fb80d06f68afe0bc307e016769b688c47e8696
ncheymbamalu <nchey.learnings@gmail.com>
2024-06-26 22:39:35 -0700

    changing code to read YouTube channel IDs instead of URLs

--- src/main.py
+++ src/main.py
@@ -2,8 +2,6 @@ import time
 
 import polars as pl
 
-from pytube import Channel
-
 from src.config import Config, load_config
 from src.logger import logging
 from src.utils import encode_transcripts, extract_transform_load
@@ -15,14 +13,9 @@ EMBEDDINGS_FILE: str = "transcript_embeddings.parquet"
 def main() -> None:
     start: float = time.time()
 
-    # a list of YouTube channel IDs
-    youtube_channel_ids: list[str] = [
-        Channel(url).channel_id for url in load_config().youtube_channel_urls
-    ]
-
     # a list of pl.LazyFrames, one per YouTube channel ID
     lazyframes: list[pl.LazyFrame] = [
-        extract_transform_load(channel_id) for channel_id in youtube_channel_ids
+        extract_transform_load(channel_id) for channel_id in load_config().youtube_channel_ids
     ]
 
     # vertically concatenate the list of pl.LazyFrames into a single pl.LazyFrame, ...
